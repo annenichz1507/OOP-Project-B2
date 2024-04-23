a558616eed7759c08d6210d42d7cd470bb4052cc
SOKHEANG.BOTH <SOKHEANG.BOTH@PNC.LAN>
2024-04-23 09:01:10 +0700

    create file

---
+++ passengers.ts
@@ -0,0 +1,13 @@
+class Passenger{
+    private name : string;
+    private age : number;
+    private gender : string;
+    private id : number;
+
+    constructor(name : string, age : number, gender : string, id : number){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+    }
+}
