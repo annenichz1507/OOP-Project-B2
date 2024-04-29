3c062848748459fd3eeb277493c5c095f176dc99
MeyHeang <sokheang.men@student.passerellesnumeriques.org>
2024-04-29 16:23:50 +0700

    create class airport #7

---
+++ src/Airport/Airport.ts
@@ -0,0 +1,8 @@
+export class Airport{
+    private name:string;
+    private address:string;
+    constructor(name:string, address:string){
+        this.name = name;
+        this.address = address;
+    }
+}
