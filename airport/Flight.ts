93085eacc4643d92c4516e2096314d089c67ea96
MeyHeang <sokheang.men@student.passerellesnumeriques.org>
2024-04-29 14:06:12 +0700

    [feat] class flight #6

---
+++ airport/Flight.ts
@@ -0,0 +1,17 @@
+export class Flight{
+    private flightName: string
+    private flightID: number
+    private flightPrice: number
+    private departure: string
+    private destination: string
+    private schedule: string
+    constructor (flightName: string, flightID: number, flightPrice: number, departure: string, destination: string, schedule: string){
+        this.flightName = flightName
+        this.flightID = flightID
+        this.flightPrice = flightPrice
+        this.departure = departure
+        this.destination = destination
+        this.schedule = schedule
+    }
+
+}
