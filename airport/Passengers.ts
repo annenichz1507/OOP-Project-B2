37821095047cab9c9bec3ef228d4aeceb148bce5
SOKHEANG.BOTH <SOKHEANG.BOTH@PNC.LAN>
2024-04-29 14:28:19 +0700

    change_path_fild

--- airport/Passengers.ts
+++ airport/Passengers.ts
@@ -1,4 +1,8 @@
-import { Person } from "./person";
-export class Passenger extends Person{
-    
+import { Person } from "./Person";
+export class Passenger extends Person {
+    private baggage: Baggage[] = [];
+    constructor(id: number, name: string, age: number, gender: boolean,
+        nationality: string, phoneNumber: number, weight: number, baggage: Baggage[]) {
+        super(id, name, age, gender, nationality, phoneNumber, weight);
+    }
 }
--- airport/person.ts
+++ airport/person.ts
@@ -2,12 +2,13 @@ export abstract class Person {
     private id : number;
     private name : string;
     private age : number;
-    private gender : string;
+    private gender : boolean;
     private nationality : string;
     private phoneNumber : number;
     private weight : number;
 
-    constructor(id : number, name : string, age : number, gender : string, nationality : string, phoneNumber : number, weight : number) {
+    constructor(id : number, name : string, age : number, gender : boolean, nationality : string,
+         phoneNumber : number, weight : number) {
         this.id = id;
         this.name = name;
         this.age = age;
