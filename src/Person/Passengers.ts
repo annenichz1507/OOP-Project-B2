e0d047c71cb7e4123fb300d70bf0e23ab8e85ae3
SOKHEANG.BOTH <SOKHEANG.BOTH@PNC.LAN>
2024-04-29 14:45:44 +0700

    create class passenger

--- src/Person/Passengers.ts
+++ src/Person/Passengers.ts
@@ -1,4 +1,8 @@
-import { Person } from "./person";
+import { Person } from "./Person";
 export class Passenger extends Person{
-    
+    private baggage: Baggage[]=[];
+    constructor(id : number, name : string, age : number, gender : string, nationality : string, phoneNumber : number, weight : number, baggage: Baggage[]){
+        super(id, name, age, gender, nationality, phoneNumber, weight);
+        this.baggage = baggage;
+    }
 }
