e97065ba6e2e4be55dbbbf2c311331e60183299f
CHANNICH NOEURN <annenichz1507>
2024-04-29 16:28:01 +0700

    create_class_boarding_pass #4

---
+++ src/Ticket/Boarding_Pass.ts
@@ -0,0 +1,22 @@
+import { Passenger } from "../Person/Passengers";
+import { Seat } from "../Airport/Seat";
+import { Gate } from "../Airport/Gate";
+
+class Boarding_Pass {
+    private passengerName: Passenger[];
+    private seats: Seat[];
+    private flightNumber: string;
+    private from: string;
+    private to: string;
+    private gateNumber: Gate;
+
+    constructor(passengerName: Passenger[], seats: Seat[], from: string, 
+    to: string, flightNumber: string, gateNumber: Gate) {
+        this.passengerName = passengerName;
+        this.seats = seats;
+        this.flightNumber = flightNumber;
+        this.from = from;
+        this.to = to;
+        this.gateNumber = gateNumber;
+    }
+}
