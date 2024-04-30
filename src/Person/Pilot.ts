eb45a8d8cabfe39efa318738b09b0d3b2a1d29e4
CHANNICH NOEURN <annenichz1507>
2024-04-30 14:36:37 +0700

    fix_bug

--- src/Person/Pilot.ts
+++ src/Person/Pilot.ts
@@ -2,7 +2,7 @@ import { Person } from "./Person";
 export class Pilot extends Person {
     private position: string;
     private salary: number;
-    constructor(position: string,salary: number, id: number, name: string, age: number, gender: string,
+    constructor(position: string,salary: number, id: number, name: string, age: number, gender: boolean,
         nationality: string, phoneNumber: number, weight: number) {
         super(id, name, age, gender, nationality, phoneNumber, weight);
         this.position = position;
