70238001cc45c8c7b57711d9b478d75a73f3a2ef
SOKHEANG.BOTH <SOKHEANG.BOTH@pnc.lan>
2024-05-03 22:56:31 +0700

    change code

--- src/Person/Employee.ts
+++ src/Person/Employee.ts
@@ -1,3 +1,7 @@
+import { Pilot } from "./Pilot";
+import { CoPilot } from "./CoPilot";
+import { FlightAttendant } from "./FlightAttendent";
+import { Chef } from "./Chef";
 export class Employee{
     
 }
